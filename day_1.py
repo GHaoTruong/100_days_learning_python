23f6eb414584aa97580e4a3ea69ddab5b1d114b3
Jack Truong <182292159+GHaoTruong@users.noreply.github.com>
2025-07-19 11:12:05 +0700

    Day 1

---
+++ day_1.py
@@ -0,0 +1,11 @@
+# "print" function will output to the terminal
+
+# print("Hello World") will output Hello world to the terminal
+print("Hello World")
+
+# input function is where it takes user input 
+Name = input("what is your name ? ")
+print("Hello " + Name)
+
+
+
