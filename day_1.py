8289242a8afdd0d82a32c48d168bcc13febf0b53
Jack Truong <182292159+GHaoTruong@users.noreply.github.com>
2025-07-25 14:03:37 +0700

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -4,8 +4,16 @@
 print("Hello World")
 
 # input function is where it takes user input 
-Name = input("what is your name ? ")
-print("Hello " + Name)
+print("Hello " + input("What is your name ?") + "!")
 
+# Variable a stored value
+name = input("What is your name? ")
+print(name)
+print("hello " + name + "!")  
 
+# But a Variable can be changed
+# name = "Emmy"
+
+# Len function shows how many latter there are in the string
+print(len(input("This shows how many letter there are in this string: ")))
 
